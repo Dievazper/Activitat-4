1e9e3f6a52e4e02d9acb5e90b5fe4fed6c39daf8
Diego Vazquez Perez <dievazper@gmail.com>
2025-09-26 11:20:40 +0200

    Add files via upload

---
+++ RA1_1.2_9_Diego_Vazquez.py
@@ -0,0 +1,15 @@
+# Autor: Diego Vazquez Perez
+# Data: 25-09-25
+
+# Escriu un programa que demani a l’usuari un número , digui si és positiu, negatiu o zero i
+# imprimeixi “Gràcies!” al final
+
+input_num = float(input("Introdueix un número: "))
+if input_num > 0:
+    print("El número és positiu.")
+if input_num < 0:
+    print("El número és negatiu.")
+if input_num == 0:
+    print("El número és zero.")
+
+print("Gràcies!")
